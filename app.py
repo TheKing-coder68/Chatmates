777065691a0d1f3ddb6d349a17e67405fc058cc7
Latkecrszy <setharaphael@gmail.com>
2021-04-08 13:29:15 -0700

    added signup functionality

--- app.py
+++ app.py
@@ -7,28 +7,31 @@ from cryptography.fernet import Fernet
 from datetime import timedelta
 
 app = Flask(__name__)
-<<<<<<< HEAD
-app.permanent_session_lifetime=timedelta(minutes=5)
-=======
 dotenv.load_dotenv()
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI', None)
 mongo = PyMongo(app)
 
 
 
->>>>>>> 1f0bc3775c2e3813aeae452f224393c615701e28
 @app.route("/")
 def main():
     return "main route"
 
-@app.route("/signup", methods=["POST","GET"])
+
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
-    if request.method=="POST":
-        session.permanent= True
-        first_name = request.form["FirstName"]
-        session["first_name"] = first_name
-    return render_template("signup.html")
+    if request.method == "POST":  # Check to make sure it's a post
+        login = mongo.db.login  # Get the login collection
+        email = request.form.get("email").lower()  # Get the person's email
+        if not re.search('^[^@ ]+@[^@ ]+\.[^@ .]{2,}$', email):  # Check to make sure they entered a valid email
+            return render_template("signup.html", error="Please enter a valid email")  # Return an error message if they didn't (will be implemented later)
+        if login.find_one({'email': email}) is not None:  # Make sure their email does not already exist
+            return render_template('signup.html', error='This email is already associated with an account')  # Return an error message if it does
+        login.insert_one({"firstName": request.form.get("firstName"), "lastName": request.form.get("lastName"),
+                          "email": email, "username": request.form.get("username")})  # Create a new document in the login database with all their info
+        return render_template("index.html")
+    return render_template("signup.html")  # If none of the other stuff happens, take them to the sign up page
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
