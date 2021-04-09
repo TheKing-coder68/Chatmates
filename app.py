0c8bcc10a5ae3b0abca8dba198080ebd1c7dc2b8
King <emokpairegenerous@gmail.com>
2021-04-09 11:56:49 -0400

    Added flash msg code into app.py and signup.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify, request, render_template, redirect, session
+from flask import Flask, make_response, jsonify, request, render_template, redirect, session, flash, url_for
 from flask_pymongo import PyMongo
 import json, os, dotenv, base64, re, argon2, random, string
 from argon2 import PasswordHasher
@@ -9,6 +9,7 @@ from datetime import timedelta
 app = Flask(__name__)
 dotenv.load_dotenv()
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI', None)
+app.config['SECRET_KEY'] = "23435khhdajk983hagsdh134dsda745112dsfksa93assd0sas8s"
 mongo = PyMongo(app)
 
 
@@ -21,16 +22,28 @@ def main():
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":  # Check to make sure it's a post
+    
         login = mongo.db.login  # Get the login collection
+    
         email = request.form.get("email").lower()  # Get the person's email
+
         if not re.search('^[^@ ]+@[^@ ]+\.[^@ .]{2,}$', email):  # Check to make sure they entered a valid email
-            return render_template("signup.html", error="Please enter a valid email")  # Return an error message if they didn't (will be implemented later)
+
+            flash("The Email that you entered is not valid, please try again.")  # Return an error message if they didn't (will be implemented later)
+
         if login.find_one({'email': email}) is not None:  # Make sure their email does not already exist
-            return render_template('signup.html', error='This email is already associated with an account')  # Return an error message if it does
+            
+            flash("This email is already associated with an account.")  # Return an error message if it does
+            return redirect('/signup')
+
         login.insert_one({"firstName": request.form.get("firstName"), "lastName": request.form.get("lastName"),
+
                           "email": email, "username": request.form.get("username"),
+
                           "password": request.form.get("password")})  # Create a new document in the login database with all their info
+
         return render_template("index.html")
+
     return render_template("signup.html")  # If none of the other stuff happens, take them to the sign up page
 
 
